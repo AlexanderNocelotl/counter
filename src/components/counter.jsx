cf0bbedf47c85916ffb30da9c066bcabb23785ae
TXT-82 <txt.hackathon@TXTs-MBP-5.askey.com>
2023-06-27 19:50:42 -0700

    firstcommit

---
+++ src/components/counter.jsx
@@ -0,0 +1,6 @@
+export default function Counter(props){
+
+return(
+    <h1 style={{textAlign:"center"}}>      {props.count} Alexander Nocelotl(s) </h1>
+)
+}
---
+++ src/components/logo.jsx
@@ -0,0 +1,8 @@
+const Logo = () =>{
+    return(
+        <img src="https://i.pinimg.com/736x/18/c9/b1/18c9b15702de5bcff4c02274c81279fd.jpg" alt="duck logo" style={{margin:"auto", position:"relative", display:"block"}}/>
+    )
+  
+    
+}
+export default Logo 
